ca989287fd19ed080f359dac870f9be8af73fd9e
ngocphule6799 <ngocphule6799@gmail.com>
2022-06-19 12:42:20 +0700

    add function plus, minus, multiple, div

--- Calculator-Exercise/Calculator-Exercise/ViewController.swift
+++ Calculator-Exercise/Calculator-Exercise/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var num1:Int?
+    var num2:Int?
+    var result:Int?
+    var resultDiv:Double?
+    var functionChoose:Int = 0
+    var updateLabelNum2:Bool = false
+    
     @IBOutlet weak var btn6: UIButton!
     @IBOutlet weak var btn5: UIButton!
     @IBOutlet weak var btn4: UIButton!
@@ -38,11 +45,37 @@ class ViewController: UIViewController {
 
     @IBAction func btnPressed(_ sender: UIButton) {
         let btnTxt:String = sender.titleLabel!.text!
-        
         if Int(btnTxt) != nil && lblResult.text!.count < 25 {
             updateLabel(num: btnTxt)
+        } else if btnTxt == "AC" {
+            lblResult.text = "0"
+        } else {
+            switch(btnTxt){
+            case "+":
+                clickFunctionButton(chooseNum: 1, btn: sender)
+                break
+            case "-":
+                clickFunctionButton(chooseNum: 2, btn: sender)
+                break
+            case "x":
+                clickFunctionButton(chooseNum: 3, btn: sender)
+                break
+            case "÷":
+                clickFunctionButton(chooseNum: 4, btn: sender)
+                break
+            case "=":
+                configAllButton()
+                updateNum2()
+                calculate()
+                break
+            case "%":
+                break
+            case "±":
+                break
+            default:
+                break
+            }
         }
-        
         
     }
     
@@ -76,13 +109,62 @@ class ViewController: UIViewController {
         configLayoutButton(button: btnEqual)
     }
     
+    func changeBoderColor(button: UIButton){
+        button.layer.borderWidth = 3
+        button.layer.borderColor = UIColor.white.cgColor
+    }
+    
     func updateLabel(num:String){
-        if (lblResult.text == "0"){
+        let txt:String = lblResult.text!
+        if txt == "0" || updateLabelNum2 == true{
             lblResult.text = num
+            updateLabelNum2 = false
         } else {
             let newString = lblResult.text! + num
             lblResult.text = newString
         }
     }
+    
+    func clickFunctionButton(chooseNum: Int, btn: UIButton){
+        configAllButton()
+        updateNum1()
+        functionChoose = chooseNum
+        changeBoderColor(button: btn)
+        updateLabelNum2 = true
+    }
+    
+    func updateNum1(){
+        num1 = Int(lblResult.text!)
+    }
+    
+    func updateNum2(){
+        num2 = Int(lblResult.text!)
+    }
+    
+    func calculate(){
+        if num1 != nil && num2 != nil {
+            switch(functionChoose){
+            case 1:
+                result = num1! + num2!
+                lblResult.text = String(result!)
+                break
+            case 2:
+                result = num1! - num2!
+                lblResult.text = String(result!)
+                break
+            case 3:
+                result = num1! * num2!
+                lblResult.text = String(result!)
+                break
+            case 4:
+                resultDiv = Double(num1!) / Double(num2!)
+                lblResult.text = String(resultDiv!)
+                break
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
