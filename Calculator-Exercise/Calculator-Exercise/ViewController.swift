9f418edeae2d92698ed340f643c1abeee6ef8834
Ngọc Phú Lê <74910907+ngocphule060719@users.noreply.github.com>
2022-06-19 10:18:10 +0700

    Auto Layout button and label
    Create button, label and auto layout

---
+++ Calculator-Exercise/Calculator-Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator-Exercise
+//
+//  Created by Le Ngoc Phu on 19/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btn0: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
