d28ba20034d111bc71109d59090c432a38a7a01e
ngocphule6799 <ngocphule6799@gmail.com>
2022-06-19 10:41:48 +0700

    mapping, add conner for button

--- Calculator-Exercise/Calculator-Exercise/ViewController.swift
+++ Calculator-Exercise/Calculator-Exercise/ViewController.swift
@@ -8,13 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btnComma: UIButton!
     @IBOutlet weak var btn0: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btnAC: UIButton!
+    @IBOutlet weak var btnPlusOrMinus: UIButton!
+    @IBOutlet weak var btnPercent: UIButton!
+    @IBOutlet weak var btnEqual: UIButton!
+    @IBOutlet weak var btnPlus: UIButton!
+    @IBOutlet weak var btnMinus: UIButton!
+    @IBOutlet weak var btnMul: UIButton!
+    @IBOutlet weak var btnDiv: UIButton!
+    @IBOutlet weak var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configAllButton()
     }
-
+    
+    
+    func configLayoutButton(button: UIButton){
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func configAllButton(){
+        configLayoutButton(button: btn0)
+        configLayoutButton(button: btn1)
+        configLayoutButton(button: btn2)
+        configLayoutButton(button: btn3)
+        configLayoutButton(button: btn4)
+        configLayoutButton(button: btn5)
+        configLayoutButton(button: btn6)
+        configLayoutButton(button: btn7)
+        configLayoutButton(button: btn8)
+        configLayoutButton(button: btn9)
+        configLayoutButton(button: btnComma)
+        configLayoutButton(button: btnPlus)
+        configLayoutButton(button: btnPlus)
+        configLayoutButton(button: btnMinus)
+        configLayoutButton(button: btnMul)
+        configLayoutButton(button: btnDiv)
+        configLayoutButton(button: btnAC)
+        configLayoutButton(button: btnPlusOrMinus)
+        configLayoutButton(button: btnPercent)
+        configLayoutButton(button: btnEqual)
+    }
 
 }
 
