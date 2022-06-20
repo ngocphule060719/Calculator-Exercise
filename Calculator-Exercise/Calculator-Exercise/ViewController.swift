c4b681751f3c907a0f6f42ec8b81299d6a4781a0
ngocphule6799 <ngocphule6799@gmail.com>
2022-06-20 19:54:58 +0700

    delete the print function (check the label)

--- Calculator-Exercise/Calculator-Exercise/ViewController.swift
+++ Calculator-Exercise/Calculator-Exercise/ViewController.swift
@@ -202,7 +202,6 @@ class ViewController: UIViewController {
     
     func checkNumberAndUpdateLabel(num: Double) {
         var newString = String(num)
-        print(newString)
         if num >= 1 || num <= -1 {
             newString = replaceDot0(txt: newString)
         }
